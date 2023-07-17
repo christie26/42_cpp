8b4f314c43c2f3da1dc90ab17c9276e1eb8e4a08
christie26 <yoonseo9800@gmail.com>
2023-07-17 22:48:40 +0900

    merge 0-1

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #include "Contact.hpp"
 
@@ -11,27 +10,3 @@ void Contact::setFirstname(std::string first_name) {
 std::string Contact::getFirstname() {
 	  return first_name;
 }
-=======
-#include "Contact.hpp"	
-	
-void setFirstname(char *first_name)
-{
-	Contact.First_name = first_name;
-}
-void setLastname(char *last_name);
-void setNickname(char *nickname);
-void setFirstname(int phone_numer);
-void setSecret(char *darkest_secret);
-
-int first_name::getFirstname()
-{
-	return First_name;
-} 
-
-
-char *getFirstname();
-char *getLastname();
-char *getNickname();
-char *getFirstname();
-char *getSecret();
->>>>>>> 97ddabb449f67ffe17291195315ec8d013ec51f9
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 #include "Contact.hpp"
 #include <iostream>
@@ -10,18 +9,3 @@ int main() {
   std::cout << myContact.getFirstname() << std::endl;
   return 0;
 }
-=======
-#include <iostream>
-
-int main(int argc, char **argv) 
-{
-	if (argc != 2)
-		std::cout << "Put only one input";
-	else
-		for (int i = 1; i < argc; i++)
-			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)toupper(argv[i][j]);
-	std::cout << std::endl;
-	return (0);
-}
->>>>>>> 97ddabb449f67ffe17291195315ec8d013ec51f9
