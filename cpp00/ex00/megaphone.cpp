9d57efdfec008e4af7246aa4225837b9e05a284a
yoonsele <yoonseo9800@gmail.com>
2023-07-23 17:07:29 +0900

    Makefile: CPP -> CXX

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 	else
 		for (int i = 1; i < argc; i++)
 			for (int j = 0; argv[i][j]; j++)
-				std::cout << (char)toupper(argv[i][j]);
+				std::cout << static_cast<float>(std::toupper(argv[i][j]));
 	std::cout << std::endl;
 	return (0);
 }
