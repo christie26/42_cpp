7f179e82a45ee80dde59eae847cf9aa660b3712e
christie26 <yoonseo9800@gmail.com>
2023-09-16 15:53:16 +0900

    implement canonical, add log option in Makefile

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -8,49 +8,45 @@
 #include <list>
 #include <vector>
 
-// template <typename T> 
-// class MutantStack : public std::stack<T> {
-
-// public:
-// 	MutantStack() : std::stack<T>() {};
- 
-//     typedef typename std::stack<T>::container_type::iterator				iterator;
-//     typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
-//     typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
-//     typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
-
-// 	iterator begin()				{ return std::stack<T>::c.begin(); }
-// 	iterator end()					{ return std::stack<T>::c.end(); }
-// 	const_iterator begin() const	{ return std::stack<T>::c.begin(); }
-// 	const_iterator end() const		{ return std::stack<T>::c.end(); }
-
-// 	reverse_iterator rbegin()				{ return std::stack<T>::c.rbegin(); }
-// 	reverse_iterator rend()					{ return std::stack<T>::c.rend(); }
-// 	const_reverse_iterator rbegin() const	{ return std::stack<T>::c.rbegin(); }
-// 	const_reverse_iterator rend() const		{ return std::stack<T>::c.rend(); }
-
-// // container_type: underlying containter type 
-// // it can be deque or vector depend on the way to initialize stack
-
-// // c: the underlying container (protected member object)
-
-// };
-
-
+/*
+By default, if no container class is specified for a particular stack class instantiation, 
+the standard container std::deque is used.
+*/
 template <typename T, typename Container = std::deque<T> >
 class MutantStack : public std::stack<T, Container> {
 public:
-    // Define your own iterator types based on the container
+    // Canonical Form
+    MutantStack() : std::stack<T, Container>() {
+        #ifdef LOG
+        std::cout << "constructor is called" << std::endl;
+        #endif
+    }
+    MutantStack(const MutantStack& copy) : std::stack<T, Container>(copy) {
+        #ifdef LOG
+        std::cout << "copy constructor is called" << std::endl;
+        #endif
+    }
+    MutantStack& operator= (const MutantStack& copy) {
+        #ifdef LOG
+        std::cout << "copy operator is called" << std::endl;
+        #endif
+
+        std::stack<T, Container>::operator=(copy);
+        return (*this);
+    }
+    ~MutantStack() {
+        #ifdef LOG
+        std::cout << "destructor is called" << std::endl;
+        #endif
+    }
+
+public:
     typedef typename Container::iterator iterator;
     typedef typename Container::const_iterator const_iterator;
     typedef typename Container::reverse_iterator reverse_iterator;
     typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
-    // Constructors
-    MutantStack() : std::stack<T, Container>() {}
-    MutantStack(const Container& c) : std::stack<T, Container>(c) {}
-
-    // Iterator functions
+    // Iterator 
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end(); }
 
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -7,102 +7,116 @@
 
 int main()
 {
-	/* use MutantStack */
-	std::cout << "## test MutantStack ##" << std::endl;
-	MutantStack<int> mstack;
-	// MutantStack<int, std::stack<int> > mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << "top: " << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << "size: " << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(737);
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-		std::cout << *it << std::endl;
-		++it;
-	}
-	std::stack<int> s1(mstack);
-	std::cout << "top: " << s1.top() << std::endl;
+	MutantStack<int> first;
+	first.push(5);
+
+	MutantStack<int> second(first);
+	MutantStack<int> third;
+	third = second;
 	std::cout << std::endl;
+
+	/* use MutantStack */
+	{	
+		std::cout << "## test MutantStack ##" << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(1);
+		mstack.push(2);
+		std::cout << "top: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "size: " << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(4);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s1(mstack);
+		std::cout << "top: " << s1.top() << std::endl;
+		std::cout << std::endl;
+	}
 
 	/* use deque */
-	std::cout << "## test deque ##" << std::endl;
-	std::deque<int> mydeque;
-	mydeque.push_back(5);
-	mydeque.push_back(17);
-	std::cout << "back: " << mydeque.back() << std::endl;
-	mydeque.pop_back();
-	std::cout << "size: " << mydeque.size() << std::endl;
-	mydeque.push_back(3);
-	mydeque.push_back(737);
-	mydeque.push_back(0);
-	MutantStack<int>::iterator it2 = mydeque.begin();
-	MutantStack<int>::iterator ite2 = mydeque.end();
-	++it2;
-	--it2;
-	while (it2 != ite2)
 	{
-		std::cout << *it2 << std::endl;
+		std::cout << "## test deque ##" << std::endl;
+		std::deque<int> mydeque;
+		mydeque.push_back(1);
+		mydeque.push_back(2);
+		std::cout << "back: " << mydeque.back() << std::endl;
+		mydeque.pop_back();
+		std::cout << "size: " << mydeque.size() << std::endl;
+		mydeque.push_back(3);
+		mydeque.push_back(4);
+		MutantStack<int>::iterator it2 = mydeque.begin();
+		MutantStack<int>::iterator ite2 = mydeque.end();
 		++it2;
+		--it2;
+		while (it2 != ite2)
+		{
+			std::cout << *it2 << std::endl;
+			++it2;
+		}
+		// both work!
+		std::stack<int, std::deque<int> > s2(mydeque);
+		// MutantStack<int> s2(mydeque);
+		std::cout << "top: " << s2.top() << std::endl;
+		std::cout << std::endl;
 	}
-	std::stack<int> s2(mydeque);
-	std::cout << "top: " << s2.top() << std::endl;
-	std::cout << std::endl;
-// we can initialize stack with deque. 
 
 	/* use list */
-	std::cout << "## test list ##" << std::endl;
-	std::list<int> mylist;
-	mylist.push_back(5);
-	mylist.push_back(17);
-	std::cout << "back: " << mylist.back() << std::endl;
-	mylist.pop_back();
-	std::cout << "size: " << mylist.size() << std::endl;
-	mylist.push_back(3);
-	mylist.push_back(737);
-	mylist.push_back(0);
-	MutantStack<int, std::list<int> >::iterator it3 = mylist.begin();
-	// std::list<int>::iterator it3 = mylist.begin();
-	std::list<int>::iterator ite3 = mylist.end();
-	++it3;
-	--it3;
-	while (it3 != ite3)
 	{
-		std::cout << *it3 << std::endl;
+		std::cout << "## test list ##" << std::endl;
+		std::list<int> mylist;
+		mylist.push_back(1);
+		mylist.push_back(2);
+		std::cout << "back: " << mylist.back() << std::endl;
+		mylist.pop_back();
+		std::cout << "size: " << mylist.size() << std::endl;
+		mylist.push_back(3);
+		mylist.push_back(4);
+		MutantStack<int, std::list<int> >::iterator it3 = mylist.begin();
+		MutantStack<int, std::list<int> >::iterator ite3 = mylist.end();
 		++it3;
+		--it3;
+		while (it3 != ite3)
+		{
+			std::cout << *it3 << std::endl;
+			++it3;
+		}
+		std::stack<int, std::list<int> > s3(mylist);
+		// MutantStack<int, std::list<int> > s3(mylist);
+		std::cout << "top: " << s3.top() << std::endl;
+		std::cout << std::endl;
 	}
-	std::stack<int, std::list<int> > s3(mylist);
-	std::cout << "top: " << s3.top() << std::endl;
-	return 0;
-
+	
 	/* use vector */
-	std::cout << "## test vector ##" << std::endl;
-	std::vector<int> myvector;
-	myvector.push_back(5);
-	myvector.push_back(17);
-	std::cout << "back: " << myvector.back() << std::endl;
-	myvector.pop_back();
-	std::cout << "size: " << myvector.size() << std::endl;
-	myvector.push_back(3);
-	myvector.push_back(737);
-	myvector.push_back(0);
-	MutantStack<int, std::vector<int> >::iterator it4 = myvector.begin();
-	MutantStack<int, std::vector<int> >::iterator ite4 = myvector.end();
-	++it4;
-	--it4;
-	while (it4 != ite4)
 	{
-		std::cout << *it4 << std::endl;
+		std::cout << "## test vector ##" << std::endl;
+		std::vector<int> myvector;
+		myvector.push_back(1);
+		myvector.push_back(2);
+		std::cout << "back: " << myvector.back() << std::endl;
+		myvector.pop_back();
+		std::cout << "size: " << myvector.size() << std::endl;
+		myvector.push_back(3);
+		myvector.push_back(4);
+		MutantStack<int, std::vector<int> >::iterator it4 = myvector.begin();
+		MutantStack<int, std::vector<int> >::iterator ite4 = myvector.end();
 		++it4;
+		--it4;
+		while (it4 != ite4)
+		{
+			std::cout << *it4 << std::endl;
+			++it4;
+		}
+		std::stack<int, std::vector<int> > s4(myvector);
+		// MutantStack<int, std::vector<int> > s4(myvector);
+		std::cout << "top: " << s4.top() << std::endl;
 	}
-	std::stack<int, std::vector<int> > s4(myvector);
-	std::cout << "top: " << s4.top() << std::endl;
+
 	return 0;
 }
